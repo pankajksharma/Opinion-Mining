43d9e99112d244f9486a023c5bd32537a9307816
om <om@ubutnu.(none)>
2013-11-15 16:53:10 +0530

    preprocess issue

---
+++ json_test.py
@@ -0,0 +1,32 @@
+import json,os
+
+def update_occurences(type, occ):
+	for k,v in occ.iteritems():
+		try:
+			type[k] += v
+		except:
+			type[k] = v
+dirlist = os.listdir("processed-data")
+pros = {}
+cons = {}
+for d in dirlist:
+	# print d
+	f = open("processed-data/"+d)
+	data = f.read().replace('""""', '""').replace(' u"', ' "').replace('{u"', '{"')
+	f.close()
+	try:
+		data = json.loads(data)
+	except Exception,e:
+		print d
+		continue
+	update_occurences(cons, data['cons'])
+	update_occurences(pros, data['pros'])
+
+f = open("pros", "w")
+for k,v in pros.iteritems():
+	f.write(k.encode('utf8')+str(v)+'\n')
+f.close()
+fc = open("cons", "w")
+for k,v in cons.iteritems():
+	f.write(k.encode('utf8')+str(v)+'\n')
+f.close()
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,4 @@
-import os
+import os,json
 from bs4 import BeautifulSoup
 from datacleaner import DataClean
 
@@ -22,5 +22,5 @@ for l in dirlist[:len(dirlist)/3]:	#1/3 for training
 		update_occurences(cons, DataClean(review.cons.get_text()).getData())
 		update_occurences(pros, DataClean(review.pros.get_text()).getData())
 	f = open("processed-data/"+l.split('.')[0]+".json", "w")
-	f.write(str({"cons": cons, "pros" : cons}).replace('\'', '"'))
+	f.write(json.dumps({"cons": cons, "pros" : cons}))
 	f.close()
